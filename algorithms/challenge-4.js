510f28a0566724567fd9415879f0644946c9c040
Fairbuyng <emijere.richard@gmail.com>
2022-02-26 17:42:07 +0100

    Solved the algorithms

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,17 @@
-function highestSum(x, y) {}
+function highestSum(x, y) {
+    let sumX = 0;
+    let sumY = 0;
+    for (let i = 0; i < x.length; i++) {
+        sumX += x[i];
+    }
+    for (let i = 0; i < y.length; i++) {
+        sumY += y[i];
+    }
+    if (sumX > sumY) {
+        return sumX;
+    } else {
+        return sumY;
+    }
+}
 
 module.exports = highestSum
